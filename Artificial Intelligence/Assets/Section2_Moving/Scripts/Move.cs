ac085cc716c127324673f193d394373260faa4d4
Mazen Morgan <morgan.mazen42@gmail.com>
2018-02-24 23:13:42 +0200

    Vectors and Moving in A Straight Line

---
+++ Artificial Intelligence/Assets/Section2_Moving/Scripts/Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	[SerializeField] Vector3 goal = new Vector3(5,0,4);
+    [SerializeField] float speed = 10.0f;
+
+	void Start ()
+    {
+	}
+	
+	void LateUpdate () 
+	{
+        transform.Translate(goal.normalized * speed * Time.deltaTime);
+    }
+}
