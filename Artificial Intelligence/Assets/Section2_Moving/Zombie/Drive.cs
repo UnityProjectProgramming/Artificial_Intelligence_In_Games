a26d9bbdc034c48f44b8831ab7208d06efac299a
Mazen Morgan <morgan.mazen42@gmail.com>
2018-02-25 17:11:25 +0200

    Pushing the Character Forward

---
+++ Artificial Intelligence/Assets/Section2_Moving/Zombie/Drive.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drive : MonoBehaviour
+{
+
+    [SerializeField] float speed = 10.0F;
+    [SerializeField] float rotationSpeed = 100.0F;
+
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+    }
+}
---
+++ Artificial Intelligence/Assets/Section2_Moving/Zombie/MoveLocal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLocal : MonoBehaviour {
+
+    [SerializeField] Transform goal;
+    float speed = 1f;
+    float accuracy = 1.0f;
+
+	void Start ()
+    {
+		
+	}
+	
+	void LateUpdate ()
+    {
+        Vector3 lookAtGoal = new Vector3(goal.position.x, transform.position.y, goal.position.z);
+        transform.LookAt(lookAtGoal);
+
+        if(Vector3.Distance(transform.position, lookAtGoal) > accuracy)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+    }
+}
