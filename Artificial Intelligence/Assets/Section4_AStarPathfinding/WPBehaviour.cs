776febe15be21082058cb974563c0d8c4d9992b6
Mazen Morgan <morgan.mazen42@gmail.com>
2018-02-27 22:29:56 +0200

    Pathfinding through waypoints using  A* algorithm Part 1

---
+++ Artificial Intelligence/Assets/Section4_AStarPathfinding/WPBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Link
+{
+    public enum Direction { UNI, BI};
+    public GameObject node1;
+    public GameObject node2;
+    public Direction direction;
+}
+
+public class WPBehaviour : MonoBehaviour {
+
+    public GameObject[] waypoints;
+    public Link[] links;
+    public Graph graph = new Graph();
+
+	// Use this for initialization
+	void Start ()
+    {
+		if(waypoints.Length > 0)
+        {
+            foreach (GameObject wp in waypoints)
+            {
+                graph.AddNode(wp);
+            }
+
+            foreach (Link l in links)
+            {
+                graph.AddEdge(l.node1, l.node2);
+                if(l.direction == Link.Direction.BI)
+                {
+                    graph.AddEdge(l.node2, l.node1);
+                }
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        graph.debugDraw();
+	}
+}
---
+++ Artificial Intelligence/Assets/Section4_AStarPathfinding/WaypointDebug.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class WaypointDebug : MonoBehaviour {
+
+	void RenameWPs(GameObject overlook)
+	{
+		GameObject[] gos;
+	    gos = GameObject.FindGameObjectsWithTag("wp"); 
+	    int i = 1;
+	    foreach (GameObject go in gos)  
+	    { 
+	     	if(go != overlook)
+	     	{
+	     		go.name = "WP" + string.Format("{0:000}",i); 
+	     		i++; 
+	     	} 
+	    }	
+	}
+
+	void OnDestroy()
+	{
+		RenameWPs(this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		if(this.transform.parent.gameObject.name != "WayPoint") return;
+		RenameWPs(null);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.GetComponent<TextMesh>().text = this.transform.parent.gameObject.name;
+	}
+}
