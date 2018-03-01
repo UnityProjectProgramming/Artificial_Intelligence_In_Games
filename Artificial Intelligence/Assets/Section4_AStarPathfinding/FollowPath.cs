6b22ce57a54ec2478110fc5f1c46f5c53c041248
Mazen Morgan <morgan.mazen42@gmail.com>
2018-03-01 22:03:58 +0200

    From Waypoints to NavMesh

--- Artificial Intelligence/Assets/Section4_AStarPathfinding/FollowPath.cs
+++ Artificial Intelligence/Assets/Section4_AStarPathfinding/FollowPath.cs
@@ -1,67 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class FollowPath : MonoBehaviour
 {
 
     [SerializeField] GameObject wpManager;
 
-    Transform goal;
-    float speed = 5.0f;
-    float accuracy = 1.0f;
-    float rotSpeed = 2.0f;
     GameObject[] waypoints;
-    GameObject currentNode;
-    int currentWP = 0;
-    Graph graph;
+    NavMeshAgent agent;
 
 	// Use this for initialization
 	void Start ()
     {
         waypoints = wpManager.GetComponent<WPBehaviour>().waypoints;
-        graph = wpManager.GetComponent<WPBehaviour>().graph;
-        currentNode = waypoints[11];
+        agent = GetComponent<NavMeshAgent>();
 	}
 
     public void GoToHeli()
     {
-        graph.AStar(currentNode, waypoints[0]);
-        currentWP = 0;
+        agent.SetDestination(waypoints[0].transform.position);
+        //graph.AStar(currentNode, waypoints[0]);
+        //currentWP = 0;
     }
     public void GoToRuin()
     {
-        graph.AStar(currentNode, waypoints[3]);
-        currentWP = 0;
+        //graph.AStar(currentNode, waypoints[3]);
+        agent.SetDestination(waypoints[3].transform.position);
+
+        //currentWP = 0;
     }
 
     // Update is called once per frame
     void LateUpdate ()
     {
-		if(graph.getPathLength() == 0 || currentWP == graph.getPathLength())
-        {
-            return;
-        }
-
-        // The node we are closest To at this moment
-        currentNode = graph.getPathPoint(currentWP);
-        //if we are close enought to the current waypoint move to the next
-        if(Vector3.Distance(graph.getPathPoint(currentWP).transform.position, transform.position) < accuracy)
-        {
-            currentWP++;
-        }
-
-        //if we are not at the end of the path
-        if(currentWP < graph.getPathLength())
-        {
-            goal = graph.getPathPoint(currentWP).transform;
-            Vector3 lookAtGoal = new Vector3(goal.position.x, transform.position.y, goal.position.z);
-            Vector3 direction = lookAtGoal - transform.position;
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
-            transform.Translate(0, 0, speed * Time.deltaTime);
-        }
-
 
 	}
 }
