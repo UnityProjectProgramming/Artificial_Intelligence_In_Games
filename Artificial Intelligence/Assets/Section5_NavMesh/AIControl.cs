a7c6b16b803c5aef1a2d8170465c371aa823a02a
Mazen Morgan <morgan.mazen42@gmail.com>
2018-03-01 23:44:48 +0200

    NavMesh Agents Part 1

---
+++ Artificial Intelligence/Assets/Section5_NavMesh/AIControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AIControl : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+	}
+
+}
---
+++ Artificial Intelligence/Assets/Section5_NavMesh/AgentManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AgentManager : MonoBehaviour {
+
+    GameObject[] agents;
+
+	// Use this for initialization
+	void Start ()
+    {
+        agents = GameObject.FindGameObjectsWithTag("ai");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 500))
+            {
+                NavMeshPath path = new NavMeshPath();
+                
+                foreach(GameObject a in agents)
+                {
+                    NavMeshAgent agentMovement = a.GetComponent<AIControl>().agent;
+                    if(agentMovement.CalculatePath(hit.point, path) && path.status == NavMeshPathStatus.PathComplete)
+                    {
+                        agentMovement.SetDestination(hit.point);
+                    }
+                    else
+                    {
+                        print("Path Invalid.");
+                    }
+                }
+            }
+            else
+            {
+                print("Faild to raycast");
+            }
+        }
+	}
+}
